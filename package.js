61865c6724070650077b721908284d711288ecbb
Ross Martin <rmartin311@gmail.com>
2015-04-21 15:30:07 -0500

    change package.js dependencies

--- package.js
+++ package.js
@@ -9,12 +9,12 @@ Package.on_use(function (api) {
   api.export('HipaaLogger');
   api.export('Hipaa');
 
-  api.use('standard-app-packages@1.0.2', ['client','server']);
-  api.use('iron:router@0.9.4', 'client');
-  api.use('mrt:bootstrap-3@0.3.8', 'client');
-  api.use('mrt:moment@2.8.1', 'client');
-  api.use('less@1.0.9', 'client')
-  api.use('mrt:font-awesome-4-less@4.4.0', 'client')
+  api.use('standard-app-packages', ['client','server']);
+  api.use('iron:router', 'client');
+  api.use('mrt:bootstrap-3', 'client');
+  api.use('mrt:moment', 'client');
+  api.use('less', 'client')
+  api.use('mrt:font-awesome-4-less', 'client')
 
   //api.use('clinical-ui-syntax', 'client')
 
